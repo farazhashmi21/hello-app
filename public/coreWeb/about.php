faee7b957767086c5c52e51aa666b84cd3458e98
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-06-30 16:03:47 +0000

    Styling, Assets and Scripting Added;

---
+++ public/coreWeb/about.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'header.php'; // Include your header file
+?>
+<!-- Page Content-->
+        <section class="pt-4">
+            <div class="container px-lg-5">
+                <!-- Page Features-->
+                <div class="row gx-lg-5">
+                    <div class="col mb-5">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+                                <h2 class="fs-4 fw-bold">About Us</h2>
+                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit explicabo, eligendi dolores exercitationem debitis eum, maxime aliquid at reiciendis tenetur eos consequatur aut molestias corrupti suscipit illo a ullam odit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php
+        require_once 'footer.php'; // Include your footer file
+        ?>
