3487eb207766722b658190082a285a316db6b7a2
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-06-30 12:04:51 +0000

    Index File Added For The Proper Redirection;

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$location = "public/index.php";
+header("Location: $location");
+?>
+warning: This file is intended to redirect to the public directory. Please ensure that the path is correct.
+// If you see this message, it means the redirection did not work as expected.
