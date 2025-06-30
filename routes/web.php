69ef89929b05e47821924ad6c861ea9f959a76f0
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-06-30 16:39:28 +0000

    Routes Updated;

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Wellcome;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\SericesController;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', [Wellcome::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/services', [ServicesController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
