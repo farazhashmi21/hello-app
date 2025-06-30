ee540c18ad550cafa26d04d0229f33ffda28d3fe
farazhashmi21 <farazhashmi9000@yahoo.com>
2025-06-30 15:55:52 +0000

    Wellcome Controller VIEWED;

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Wellcome;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Wellcome::class, 'index']);
